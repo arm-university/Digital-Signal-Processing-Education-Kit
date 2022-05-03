a1396e489d180feff793db2f558cc2a94d7b2c20
franciscatan-armedu <94066397+franciscatan-armedu@users.noreply.github.com>
2022-05-03 15:25:30 +0800

    Delete STM32 directory
    Removin STM32 directory as this canonical version of the education kit will reside in the main branch only. Uni-adapted branch will contain all subtrees from standalone adaptations done by various uni academicians

--- STM32F746G Discovery board/contents/Module_6/Lab/Lab_Files/src/comb14.h
+++
@@ -1,5 +0,0 @@
-//comb14.h Coefficients file for comb filter with 6 bands
-
-#define	N 14  //number of coefficients h(13),h(12),...,h(0)
-
-float h[N] = {1.0,0,0,0,0,0,0,0,0,0,0,0,0,-1.0};
--- STM32F746G Discovery board/html/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).foundation()
-
-//$(document).bind("contextmenu",function(){ return false; });
-
-var AupCourse = angular.module('AupCourse', [
-	'AupControllers'
-]);
