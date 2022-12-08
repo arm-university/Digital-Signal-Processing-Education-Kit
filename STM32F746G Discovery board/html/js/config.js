adb4c0e1e55e15e4045e40b56b7a70c9f07452aa
Richard Cook <r.a.cook@soton.ac.uk>
2022-12-08 15:01:50 +0000

    Fix paths in config.js

--- STM32F746G Discovery board/html/js/config.js
+++ STM32F746G Discovery board/html/js/config.js
@@ -20,51 +20,51 @@ var index =
       {
         "column": "modules",
         "name": "Module 1: Discrete-Time Signals and Systems - Convolution and Correlation",
-        "link": "contents/Module_1/ConvolutionAndCorrelation_lec.pptx"
+        "link": "contents/Module 01_Discrete Time Signals and Systems_Convolution and Correlation/ConvolutionAndCorrelation_lec.pptx"
       },
-	  {
+    {
         "column": "exercises",
         "name": "Getting Started and Project Source Files",
-        "link": "contents/Module_1/Lab/Getting_Started.zip"
+        "link": "contents/Module 01_Discrete Time Signals and Systems_Convolution and Correlation/Lab/IntroductionLab"
       },
       {
         "column": "exercises",
         "name": "Lab Manual",
-        "link": "contents/Module_1/Lab/IntroductionLab/DSP_Lab_Manual.docx"
+        "link": "contents/Module 01_Discrete Time Signals and Systems_Convolution and Correlation/Lab/IntroductionLab/DSP_Lab_Manual.docx"
       },
-	  {
+    {
         "column": "exercises",
         "name": "Lab Exercise MATLAB data files",
-        "link": "contents/Module_1/Lab/IntroductionLab/Matlab_Lab_Files.zip"
+        "link": "contents/Module 01_Discrete Time Signals and Systems_Convolution and Correlation/Lab/IntroductionLab/Matlab_Lab_Files"
       },
       {
         "column": "exercises",
         "name": "Solutions",
-        "link": "contents/Module_1/Lab/IntroductionLab/Solution.zip"
+        "link": "contents/Module 01_Discrete Time Signals and Systems_Convolution and Correlation/Lab/IntroductionLab/Solution"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 2: Sampling, Reconstruction and Aliasing - Review of Complex Exponentials and Fourier Analysis",
-        "link": "contents/Module_2/SamplingReconstructionAndAliasing1_lec.pptx"
+        "link": "contents/Module 02_Sampling, Reconstruction and Aliasing_part 1/SamplingReconstructionAndAliasing1_lec.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 3: Sampling, Reconstruction and Aliasing - Time and Frequency Domains",
-        "link": "contents/Module_3/SamplingReconstructionAndAliasing2_lec.pptx"
+        "link": "contents/Module 03_Sampling, Reconstruction and Aliasing_part 2/SamplingReconstructionAndAliasing2_lec.pptx"
       },
-	  {
+    {
         "column": "exercises",
         "name": "Lab Manual",
-        "link": "contents/Module_3/Lab/DSP_Lab_Manual.docx"
+        "link": "contents/Module 03_Sampling, Reconstruction and Aliasing_part 2/Lab/DSP_Lab_Manual.docx"
       },
       {
         "column": "exercises",
         "name": "Solutions",
-        "link": "contents/Module_3/Lab/Solution.zip"
+        "link": "contents/Module 03_Sampling, Reconstruction and Aliasing_part 2/Lab/Solution"
       }
 
     ],
@@ -72,36 +72,36 @@ var index =
       {
         "column": "modules",
         "name": "Module 4: Time and Frequency Domains - z-transform",
-        "link": "contents/Module_4/Z-Transform_lec.pptx"
+        "link": "contents/Module 04_Z-Transform_Time and Frequency Domains/Z-Transform_lec.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 5: FIR Filters - Moving Average Filters",
-        "link": "contents/Module_5/FIR1_lec.pptx"
+        "link": "contents/Module 05_FIR Filters_part 1/FIR1_lec.pptx"
       }
     ],
     [
-	  {
+    {
         "column": "modules",
         "name": "Module 6: FIR Filters - Window Method of Design",
-        "link": "contents/Module_6/FIR2_lec.pptx"
+        "link": "contents/Module 06_FIR Filters_part 2/FIR2_lec.pptx"
       },
       {
         "column": "exercises",
         "name": "Lab Manual",
-        "link": "contents/Module_6/Lab/DSP_Lab_Manual.docx"
+        "link": "contents/Module 06_FIR Filters_part 2/Lab/DSP_Lab_Manual.docx"
       },
-	  {
+    {
         "column": "exercises",
         "name": "Lab Exercise files",
-        "link": "contents/Module_6/Lab/Lab_Files.zip"
+        "link": "contents/Module 06_FIR Filters_part 2/Lab/Lab_Files"
       },
       {
         "column": "exercises",
         "name": "Solutions",
-        "link": "contents/Module_6/Lab/Solution.zip"
+        "link": "contents/Module 06_FIR Filters_part 2/Lab/Solution"
       }
 
     ],
@@ -109,89 +109,89 @@ var index =
       {
         "column": "modules",
         "name": "Module 7: IIR Filters - Impulse Invariant and Bilinear Transform Methods of Design",
-        "link": "contents/Module_7/IIR1_lec.pptx"
+        "link": "contents/Module 07_IIR Filters_part 1/IIR1_lec.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 8: IIR Filters - Simple IIR Filter Design Example",
-        "link": "contents/Module_8/IIR2_lec.pptx"
+        "link": "contents/Module 08_IIR Filters_part 2/IIR2_lec.pptx"
       },
       {
         "column": "exercises",
         "name": "Lab Manual",
-        "link": "contents/Module_8/Lab/DSP_Lab_Manual.docx"
+        "link": "contents/Module 08_IIR Filters_part 2/Lab/DSP_Lab_Manual.docx"
       },
       {
         "column": "exercises",
         "name": "Lab Exercise files",
-        "link": "contents/Module_8/Lab/Lab_Files.zip"
+        "link": "contents/Module 08_IIR Filters_part 2/Lab/Lab_Files"
       },
       {
         "column": "exercises",
         "name": "Solutions",
-        "link": "contents/Module_8/Lab/Solution.zip"
+        "link": "contents/Module 08_IIR Filters_part 2/Lab/Solution"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 9: Fast Fourier Transform - Review of Fourier Analysis",
-        "link": "contents/Module_9/FFT1_lec.pptx"
+        "link": "contents/Module 09_Fast Fourier Transform_part 1/FFT1_lec.pptx"
       }
     ],
     [
-	  {
+    {
         "column": "modules",
         "name": "Module 10: Fast Fourier Transform - Derivation of Radix-2 FFT",
-        "link": "contents/Module_10/FFT2_lec.pptx"
+        "link": "contents/Module 10_Fast Fourier Transform_part 2/FFT2_lec.pptx"
       },
       {
         "column": "exercises",
         "name": "Lab Manual",
-        "link": "contents/Module_10/Lab/DSP_Lab_Manual.docx"
+        "link": "contents/Module 10_Fast Fourier Transform_part 2/Lab/DSP_Lab_Manual.docx"
       },
       {
         "column": "exercises",
         "name": "Solutions",
-        "link": "contents/Module_10/Lab/Solution.zip"
+        "link": "contents/Module 10_Fast Fourier Transform_part 2/Lab/Solution"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 11: Adaptive Filters - Prediction and System Identification",
-        "link": "contents/Module_11/AdaptiveFilters1_lec.pptx"
+        "link": "contents/Module_11_Adaptive Filters_part 1/AdaptiveFilters1_lec.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 12: Adaptive Filters - Equalisation and Noise Cancellation",
-        "link": "contents/Module_12/AdaptiveFilters2_lec.pptx"
+        "link": "contents/Module_12_Adaptive Filters_part 2/AdaptiveFilters2_lec.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 13: Adaptive Filters - Adaptive FIR Filter and the LMS Algorithm",
-        "link": "contents/Module_13/AdaptiveFilters3_lec.pptx"
+        "link": "contents/Module_13_Adaptive Filters_part 3/AdaptiveFilters3_lec.pptx"
       },
       {
         "column": "exercises",
         "name": "Lab Manual",
-        "link": "contents/Module_13/Lab/DSP_Lab_Manual.docx"
+        "link": "contents/Module_13_Adaptive Filters_part 3/Lab/DSP_Lab_Manual.docx"
       },
       {
         "column": "exercises",
         "name": "Lab Exercise file",
-        "link": "contents/Module_13/Lab/Lab_File.zip"
+        "link": "contents/Module_13_Adaptive Filters_part 3/Lab/Lab_File"
       },
       {
         "column": "exercises",
         "name": "Solutions",
-        "link": "contents/Module_13/Lab/Solution.zip"
+        "link": "contents/Module_13_Adaptive Filters_part 3/Lab/Solution"
       }
     ]
   ]
